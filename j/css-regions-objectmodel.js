d2bf7b54e26275dde402fa5171e4321ab1332fab
François REMY <fremycompany_pub@yahoo.fr>
2013-08-12 17:35:12 -0700

    Fixed: typo

--- j/css-regions-objectmodel.js
+++ j/css-regions-objectmodel.js
@@ -534,7 +534,7 @@ cssRegions.enablePolyfillObjectModel = function() {
     cssCascade.polyfillStyleInterface('flow-from');
     cssCascade.polyfillStyleInterface('region-fragment');
     cssCascade.polyfillStyleInterface('break-before');
-    cssCascade.polyfillStyleInterface('break-afterx');
+    cssCascade.polyfillStyleInterface('break-after');
 
 }
 
